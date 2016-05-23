fa6e8d3b9f0a53013d6c9d463fd2dc24abbca8e2
Ákos <akoshervay@gmail.com>
2016-05-23 19:35:58 +0200

    initial processexecutor commit

---
+++ src/main/java/com/akos/gui/jfx_components/ProcessExecutor.java
@@ -0,0 +1,17 @@
+package com.akos.gui.jfx_components;
+
+import javafx.concurrent.Task;
+
+/**
+ * @author: Ákos Hervay(akoshervay@gmail.com)
+ */
+
+
+public class ProcessExecutor extends Task<Void> {
+
+
+    @Override
+    protected Void call() throws Exception {
+        return null;
+    }
+}
